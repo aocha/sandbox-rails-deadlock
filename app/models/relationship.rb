84a40e447e3b549dde5826249aaea0b21e79edf3
aocha <ao.hk215@gmail.com>
2022-12-04 15:05:52 +0900

    検証に使う処理を用意

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,22 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
+
+  def self.bulk_insert(number_of_threads:)
+    followee_ids = User.all.order(id: :asc).pluck(:id).take(5)
+    threads = [*1..100].take(number_of_threads).map do |follower_id|
+      Thread.new do
+        ApplicationRecord.transaction do
+          relationships = followee_ids.map do |followee_id|
+            Relationship.new(follower_id: follower_id, followee_id: followee_id, count: Random.rand(0..100))
+          end
+          relationships.each_slice(100) do |r|
+            Relationship.import(r, on_duplicate_key_update: [:count])
+          end
+        end
+      end
+    end
+    threads.each(&:join)
+    puts 'completed'
+  end
 end
