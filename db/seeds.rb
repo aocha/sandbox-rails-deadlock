dbed69a8ea27d8634ee9b5ff90b7742506be3117
aocha <ao.hk215@gmail.com>
2022-12-04 15:05:20 +0900

    検証に使う初期データを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = 100.times.map { |n| User.new(nickname: "nickname-#{n}") }
+User.import(users, on_duplicate_key_update: [])
+
+user_ids = User.all.order(id: :asc).pluck(:id)
+relationships = user_ids.product(user_ids).map do |follower_id, followee_id|
+  Relationship.new(follower_id: follower_id, followee_id: followee_id)
+end
+
+relationships.each_slice(1000) do |r|
+  Relationship.import(r, on_duplicate_key_update: [])
+end
